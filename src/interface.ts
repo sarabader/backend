e249aab1914d7bf185c003fe05204860f4bcce14
Sara <sara@gmail.com>
2022-11-14 16:18:44 +0300

    HW1

--- src/interface.ts
+++ src/interface.ts
@@ -1,23 +1,23 @@
 
 
-// interface USERID{
-//    name:string;
-//     Age: number;
-//     Specialization:string;
-//     bestfriends: string;
+interface USERID{
+   name:string;
+    Age: number;
+    Specialization:string;
+    bestfriends: string;
+
+}
+const user1:USERID={
+    name:'SARA',
+     Age: 23,
+     Specialization:'Student',
+     bestfriends: '1-SARA 2- mona 3-Nada',
+ 
+ }
+ console.log(user1);
+
+// function output(user1){
+//     return 'Name:'+ USERID.name + 'Age:'+ this.Age +'Specialization:' +this.Specialization +'best friends:'+ this.bestfriends ;
 
 // }
-// const user1:USERID={
-//     name:'SARA',
-//      Age: 23,
-//      Specialization:'Student',
-//      bestfriends: '1-SARA 2- mona 3-Nada',
- 
-//  }
-//  console.log(user1);
-
-// // function output(user1){
-// //     return 'Name:'+ USERID.name + 'Age:'+ this.Age +'Specialization:' +this.Specialization +'best friends:'+ this.bestfriends ;
-
-// // }
-// // console.log(output(user1));
+// console.log(output(user1));
